878ef60b07a811284f6874cfab474540a6c27615
KeerthiKamani6 <92873846+KeerthiKamani6@users.noreply.github.com>
2022-11-16 14:25:27 -0600

    made required changes

--- controllers/pen.js
+++ controllers/pen.js
@@ -91,38 +91,98 @@ exports.pen_create_post = async function (req, res) {
     }
 
 };
-// Handle Pen delete form on DELETE.
-exports.pen_delete = function (req, res) {
 
-    res.send('NOT IMPLEMENTED: Pen delete DELETE ' + req.params.id);
-
-};
 
 // Handle Pen update form on PUT.
 
 //exports.pen_update_put = function (req, res) {
 
-    //res.send('NOT IMPLEMENTED: Pen update PUT' + req.params.id);
+//res.send('NOT IMPLEMENTED: Pen update PUT' + req.params.id);
 
 //};
 
 
-exports.pen_update_put = async function(req, res) { 
+exports.pen_update_put = async function (req, res) {
     console.log(`update on id ${req.params.id} with body 
-${JSON.stringify(req.body)}`) 
-    try { 
-        let toUpdate = await pen.findById( req.params.id) 
+${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await pen.findById(req.params.id)
         // Do updates of properties 
-        if(req.body.width)  
-               toUpdate.width = req.body.width; 
-        if(req.body.color) toUpdate.color = req.body.color; 
-        if(req.body.style) toUpdate.style = req.body.style; 
-        let result = await toUpdate.save(); 
-        console.log("Sucess " + result) 
-        res.send(result) 
-    } catch (err) { 
-        res.status(500) 
+        if (req.body.width)
+            toUpdate.width = req.body.width;
+        if (req.body.color) toUpdate.color = req.body.color;
+        if (req.body.style) toUpdate.style = req.body.style;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
         res.send(`{"error": ${err}: Update for id ${req.params.id} 
-failed`); 
+failed`);
+    }
+};
+// Handle pen delete on DELETE. 
+exports.pen_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await pen.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
+};
+//Handle a show one view with id specified by query 
+exports.pen_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await pen.findById(req.query.id)
+        res.render('pendetail', { title: 'pen Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a pen. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.pen_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('pencreate', { title: 'pen Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+
+// Handle building the view for updating a pen
+// query provides the id 
+exports.pen_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await pen.findById(req.query.id) 
+        res.render('penupdate', { title: 'pen Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle a delete one view with id from query 
+exports.pen_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await pen.findById(req.query.id) 
+        res.render('pendelete', { title: 'pen Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
     } 
 }; 
--- routes/pen.js
+++ routes/pen.js
@@ -2,11 +2,22 @@ var express = require('express');
 const pen_controllers= require('../controllers/pen');
 var router = express.Router();
 
-/* GET television */ 
+/* GET pen */ 
 router.get('/', pen_controllers.pen_view_all_Page );
-  // GET request for one costume. 
-router.get('/:id', pen_controllers.pen_detail); 
-router.put('/:id', pen_controllers.pen_update_put); 
- 
+  // GET request for one pen. 
+router.get('pen/:id', pen_controllers.pen_detail); 
 
+/* GET detail pen page */ 
+router.get('/detail', pen_controllers.pen_view_one_Page); 
 module.exports = router;
+
+/* GET create pen page */ 
+router.get('/create', pen_controllers.pen_create_Page); 
+
+/* GET create update page */ 
+router.get('/update', pen_controllers.pen_update_Page); 
+
+/* GET delete pen page */ 
+router.get('/delete', pen_controllers.pen_delete_Page); 
+
+ 
