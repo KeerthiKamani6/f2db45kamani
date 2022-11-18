f2d778d11a9beadcb6d4d797c22866f7249968f6
KeerthiKamani6 <92873846+KeerthiKamani6@users.noreply.github.com>
2022-11-17 20:57:06 -0600

    update files

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var pen = require('./models/pen');
+var passport = require('passport'); 
+var LocalStrategy = require('passport-local').Strategy;
+passport.use(new LocalStrategy( 
+  function(username, password, done) { 
+    Account.findOne({ username: username }, function (err, user) { 
+      if (err) { return done(err); } 
+      if (!user) { 
+        return done(null, false, { message: 'Incorrect username.' }); 
+      } 
+      if (!user.validPassword(password)) { 
+        return done(null, false, { message: 'Incorrect password.' }); 
+      } 
+      return done(null, user); 
+    })}));
 
 require('dotenv').config(); 
 const connectionString =  
@@ -48,7 +62,22 @@ app.use('/pen', penRouter);
 app.use('/gridbuild', gridbuildRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
-
+app.use(require('express-session')({ 
+  secret: 'keyboard cat', 
+  resave: false, 
+  saveUninitialized: false 
+})); 
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+ 
+// passport config 
+// Use the existing connection 
+// The Account model  
+var Account =require('./models/account'); 
+ 
+passport.use(new LocalStrategy(Account.authenticate())); 
+passport.serializeUser(Account.serializeUser()); 
+passport.deserializeUser(Account.deserializeUser()); 
 
 
 // catch 404 and forward to error handler
@@ -96,5 +125,5 @@ style:"ball"});
       }); 
 } 
  
-let reseed = true; 
-if (reseed) { recreateDB();} 
+
+ 
